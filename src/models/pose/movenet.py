d99cffeeea39a0fc48f509108de63050a60ae68f
Sumit Kumar Jethani Jethani <sumitkumar_jethani@hotmail.com>
2023-07-02 17:05:15 +0200

    (dev) movenet keypoints dict

--- src/models/pose/movenet.py
+++ src/models/pose/movenet.py
@@ -12,6 +12,27 @@ movenet_models = {
     }
 }
 
+# Dictionary that maps from joint names to keypoint indices.
+key_point_dict = {
+    'nose': 0,
+    'left_eye': 1,
+    'right_eye': 2,
+    'left_ear': 3,
+    'right_ear': 4,
+    'left_shoulder': 5,
+    'right_shoulder': 6,
+    'left_elbow': 7,
+    'right_elbow': 8,
+    'left_wrist': 9,
+    'right_wrist': 10,
+    'left_hip': 11,
+    'right_hip': 12,
+    'left_knee': 13,
+    'right_knee': 14,
+    'left_ankle': 15,
+    'right_ankle': 16
+}
+
 
 def get_model_url(model_name):
     return movenet_models.get(model_name, {}).get("url", None)
