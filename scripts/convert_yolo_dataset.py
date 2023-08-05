9c13251a74f23ed6b7aa6cf5f3bcbca0087196da
Sumit Kumar Jethani Jethani <sumitkumar_jethani@hotmail.com>
2023-08-05 19:46:39 +0100

    convert_yolo_dataset.py script adapted

--- scripts/convert_yolo_dataset.py
+++ scripts/convert_yolo_dataset.py
@@ -1,37 +1,32 @@
 """Script to covert a yolo formated dataset into a folder like structured dataset for classification"""
 import argparse
-import logging
 import os
 import shutil
-import sys
-from typing import Dict
-
 import yaml
+from typing import Dict
+from fall_detection.logger.logger import LoggerSingleton
 
-sys.path.append("./")
-from fall_detection.logger.logger import configure_logging
-
-logger = logging.getLogger("app")
+logger = LoggerSingleton("app").get_logger()
 
 
-parser = argparse.ArgumentParser()
+def cli():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "-i",
-    "--input",
-    help="input directory to read data from.",
-    type=str,
-    required=True,
-)
-parser.add_argument(
-    "-o",
-    "--output",
-    help="output directory to save processed data.",
-    type=str,
-    required=True,
-)
-
-args = parser.parse_args()
+    parser.add_argument(
+        "-i",
+        "--input",
+        help="input directory to read data from.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="output directory to save processed data.",
+        type=str,
+        required=True,
+    )
+    return parser.parse_args()
 
 
 def get_class_names(path):
@@ -44,15 +39,12 @@ def move_images(input_dir: str, output_dir: str, class_names: Dict[int, str]):
     for file in os.listdir(os.path.join(input_dir, "labels")):
         with open(os.path.join(input_dir, "labels", file), "r") as f:
             for line in f.readlines():
-                print(line)
                 image_file = file[:-3] + "jpg"
                 src = os.path.join(input_dir, "images", image_file)
                 class_name = class_names[int(line[0])]
                 if not os.path.exists(os.path.join(output_dir, class_name)):
                     os.mkdir(os.path.join(output_dir, class_name))
                 dst = os.path.join(output_dir, class_name, image_file)
-                print(src)
-                print(dst)
                 shutil.copy(src=src, dst=dst)
 
 
@@ -74,7 +66,7 @@ def main(input_dir: str, output_dir: str):
 
 
 if __name__ == "__main__":
-    configure_logging()
+    args = cli()
 
     logger.debug(f"converting dataset from {args.input}")
     main(args.input, args.output)
