b4eb64e6fe891b49d5812552490c5081590674c1
Sumit Kumar Jethani Jethani <sumitkumar_jethani@hotmail.com>
2023-08-16 17:39:36 +0100

    evaluate pose_classifier script

--- scripts/evaluate_pose_classifier.py
+++ scripts/evaluate_pose_classifier.py
@@ -1,14 +1,10 @@
 import argparse
 import sys
+import os
 import pickle
 import numpy as np
-
-from fall_detection.logger.logger import LoggerSingleton
 from fall_detection.fall.data import load_pose_samples_from_dir
 from sklearn.metrics import classification_report, roc_auc_score
-
-
-logger = LoggerSingleton("app").get_logger()
 
 
 def get_metrics(y_test, y_pred):
@@ -22,21 +18,28 @@ def cli():
 
     parser.add_argument(
         "-i",
-        "--input_file",
+        "--input",
         help="input path to read the landmarks",
         type=str,
         required=True,
     )
     parser.add_argument(
         "-o",
-        "--output_file",
-        help="output txt file to save metrics",
+        "--output",
+        help="output path to save metrics",
         type=str,
         required=True,
     )
     parser.add_argument(
-        "-c",
-        "--classification-model",
+        "-f",
+        "--file-name",
+        help="metrics file name.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--pose-classifier",
+        "--pose-classifier",
         help="pose classification model to use.",
         type=str,
         required=True,
@@ -48,33 +51,34 @@ def main():
     try:
         args = cli()
 
-        with open(f"{args.classification_model}", "rb") as f:
-            logger.info("Loading pose classifier...")
+        with open(f"{args.pose_classifier}", "rb") as f:
+            print(f"Loading pose classifier from: {args.pose_classifier}")
             pose_classifier = pickle.load(f)
 
         # load csv file with pose samples
-        logger.info("Loading pose samples...")
+        print(f"Loading pose samples from: {args.input}")
         pose_samples = load_pose_samples_from_dir(
             pose_embedder=pose_classifier._pose_embedder,
-            landmarks_dir=args.input_file,
+            landmarks_dir=args.input,
             n_landmarks=pose_classifier._n_landmarks,
             n_dimensions=pose_classifier._n_dimensions,
         )
 
-        logger.info("Predicting pose samples...")
-        y = np.where(np.array([ps.class_name for ps in pose_samples]) == "Fall", 1, 0)
+        print("Predicting on loaded pose samples...")
+        y = np.where(np.array([ps.class_name for ps in pose_samples]) == "fall", 1, 0)
+        y_pred = np.where(pose_classifier.predict_pose_samples(pose_samples) == "fall", 1, 0)
 
-        y_pred = np.where(
-            pose_classifier.predict_pose_samples(pose_samples) == "Fall", 1, 0
-        )
+        # Create output folder if not exists.
+        if not os.path.exists(args.output):
+            os.makedirs(args.output)
 
-        logger.info(f"Writing metrics in {args.output_file} ...")
-        with open(args.output_file, "w") as f:
+        print(f"Writing metrics in {args.output} ...")
+        with open(os.path.join(args.output, f"{args.file_name}.txt"), "w") as f:
             f.write(get_metrics(y, y_pred))
 
         sys.exit(0)
     except Exception as e:
-        logger.error(str(e))
+        print(str(e))
         sys.exit(1)
 
 
